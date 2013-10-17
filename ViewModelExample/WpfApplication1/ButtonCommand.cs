1cc24227492e19fbc8dae749cb276264e896456e
qadam <kulcsar.adam1991@gmail.com>
2013-10-17 09:51:12 +0200

    ViewModel
    Példa projekt View -> ViewModel -> Model
    Előadás alatti példa

---
+++ ViewModelExample/WpfApplication1/ButtonCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApplication1
+{
+    class ButtonCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            if (parameter != null)
+            {
+                return !String.IsNullOrEmpty(parameter.ToString());
+            }
+            return false;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            MessageBox.Show("Hello " + parameter.ToString());
+        }
+
+        public ButtonCommand(INotifyPropertyChanged notify)
+        {
+            notify.PropertyChanged += notify_PropertyChanged;
+        }
+
+        void notify_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged(this, e);
+            }
+        }
+    }
+}
---
+++ ViewModelExample/WpfApplication1/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApplication1
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public ICommand ButtonComm { get; set; }        
+        public event PropertyChangedEventHandler PropertyChanged;
+        private String name;
+
+        public String Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public ViewModel()
+        {
+            ButtonComm = new ButtonCommand(this);
+        }
+    }
+}
