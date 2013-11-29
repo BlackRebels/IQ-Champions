98f6971296372c559d3f7b6c0b54faaba4029936
xavios <mr.acs.andras@gmail.com>
2013-11-29 22:26:03 +0100

    webalkalmatás kész
    bébi / bogyó a teszt

---
+++ IQChampions/WebApplication/database.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace WebApplication
+{
+    public static class database
+    {
+        static string connectionString = WebConfigurationManager.ConnectionStrings["IQChampionsEntities"].ToString();
+
+        /// <summary>
+        /// Visszaadja a játékosok neveit
+        /// </summary>
+        /// <returns></returns>
+        public static List<string> getNamesOfPlayers()
+        {
+            List<string> retVal = new List<string>();
+            string sqlCommand = "select name from dbUserSet";
+            SqlCommand comm = new SqlCommand(sqlCommand);
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                comm.Connection = conn;
+                conn.Open();
+                SqlDataReader dr = comm.ExecuteReader();
+                while (dr.Read())
+                    retVal.Add(dr["name"].ToString());
+            }
+            return retVal;
+        }
+
+        /// <summary>
+        /// Visszaadja, hogy az usernek jó-e a loginja
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="pass"></param>
+        /// <returns></returns>
+        public static bool getLoginOfUser(string name, string pass)
+        {
+            bool retVal = false;
+            SqlCommand sql = new SqlCommand("checkLogin") { CommandType = System.Data.CommandType.StoredProcedure };
+            sql.Parameters.Add(new SqlParameter("name", name));
+            sql.Parameters.Add(new SqlParameter("pass", pass));
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                sql.Connection = conn;
+                conn.Open();
+                SqlDataReader dr = sql.ExecuteReader();
+                if (dr.Read())
+                {
+                    retVal = int.Parse(dr[0].ToString()) > 0;
+                }
+            }
+            return retVal;
+        }
+
+        public static void registerUser(string name, string password, string email)
+        {
+            SqlCommand sql = new SqlCommand("regUser") { CommandType = System.Data.CommandType.StoredProcedure };
+            sql.Parameters.Add(new SqlParameter("name", name));
+            sql.Parameters.Add(new SqlParameter("pass", password));
+            sql.Parameters.Add(new SqlParameter("email", email));
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                sql.Connection = conn;
+                conn.Open();
+                sql.ExecuteNonQuery();
+            }
+        }
+
+        public static List<string> getTop5Players()
+        {
+            List<string> retVal = new List<string>();
+            SqlCommand comm = new SqlCommand("getTop5Winner") { CommandType = System.Data.CommandType.StoredProcedure };
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                comm.Connection = conn;
+                conn.Open();
+                SqlDataReader dr = comm.ExecuteReader();
+                while (dr.Read())
+                    retVal.Add(dr[0].ToString());
+            }
+            return retVal;
+        }
+
+        public static DataTable getStatistics4User(string userName)
+        {
+            DataTable retVal = new DataTable();
+            retVal.Columns.Add(new DataColumn("played", typeof(int)) {Caption = "Játszott"});
+            retVal.Columns.Add(new DataColumn("win", typeof(int)) {Caption = "Nyert"});
+            retVal.Columns.Add(new DataColumn("winPercent", typeof(double)) {Expression = "win / played", Caption = "Nyerési százalék" });
+            retVal.Columns.Add(new DataColumn("questions",typeof(int)) {Caption = "Megválaszolt kérdések"});
+            retVal.Columns.Add(new DataColumn("goodAnswers", typeof(int)) {Caption = "Helyesen megválaszolt kérdések"});
+            retVal.Columns.Add(new DataColumn("answerPercent", typeof(double)) {Expression = "goodAnswers / questions", Caption = "Százalék"});
+
+            SqlCommand comm = new SqlCommand("getStatOfUsr") {CommandType = CommandType.StoredProcedure};
+            comm.Parameters.Add(new SqlParameter("name",userName));
+            using(SqlConnection conn = new SqlConnection(connectionString))
+            {
+                comm.Connection = conn;
+                conn.Open();
+                SqlDataReader dr = comm.ExecuteReader();
+                while (dr.Read())
+                {
+                    DataRow dtr = retVal.NewRow();
+                    dtr["played"] = int.Parse(dr["played"].ToString());
+                    dtr["goodAnswers"] = int.Parse(dr["goodanswers"].ToString());
+                    dtr["questions"] = int.Parse(dr["questions"].ToString());
+                    dtr["win"] = int.Parse(dr["win"].ToString());
+                    retVal.Rows.Add(dtr);
+                }
+            }
+            return retVal;
+        }
+    }
+}
--- IQChampions/WebApplication/index.aspx
+++ IQChampions/WebApplication/index.aspx
@@ -1,7 +1,90 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/iqMaster.Master" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="WebApplication.index" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <script>
+        <asp:Literal ID="jsLiteral" runat="server"></asp:Literal>
+    </script>
+</asp:Content>
+<asp:Content ID="portletPlaceIndex" ContentPlaceHolderID="portletPlace" runat="server">
+    <asp:Panel runat="server" ID="loginPortlet" Visible="true" CssClass="loginPortlet">
+        <asp:Panel runat="server" ID="onBeforeLogin">
+            <h5>Bejelentkezés</h5>
+            <table>
+                <tr>
+                    <td>Név:</td>
+                    <td>
+                        <asp:TextBox runat="server" ID="loginPortletUname"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Jelszó:</td>
+                    <td>
+                        <asp:TextBox runat="server" ID="loginPortletPass" TextMode="Password"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center">
+                        <asp:Button runat="server" ID="submitLoginPortlet" Text="Bejelntkezés" CausesValidation="false" /></td>
+                </tr>
+            </table>
+        </asp:Panel>
+        <asp:Panel runat="server" ID="logoffPanel" Visible ="false">
+            Üdvözöljük, <asp:Label runat="server" ID="nameLabelLogOff"></asp:Label>
+            <asp:LinkButton runat="server" ID="logOffButton" Text="Kijelentkezés" OnClick="logOffButton_Click"></asp:LinkButton>
+        </asp:Panel>
+    </asp:Panel>
+    <br />
+     <asp:Panel runat="server" ID="top5" Visible ="true" CssClass="loginPortlet">
+            <h5>Legjobb 5 játékos</h5>
+            <asp:Literal runat="server" ID="lbTop5"></asp:Literal>
+        </asp:Panel>
 
 </asp:Content>
+
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    ide jön a tuti original content
+    <asp:Panel runat="server" ID="tbThankYaou" Visible="false">
+        <h3>
+            <asp:Label runat="server" ID="lbThankYouMessage"></asp:Label></h3>
+        <asp:HyperLink runat="server" ID="hlBackToMain" NavigateUrl="~/index.aspx">Vissza a főoldalra</asp:HyperLink>
+    </asp:Panel>
+    <asp:Panel ID="pnRegistration" runat="server" Visible="false">
+        <h3>Regisztráció</h3>
+        <table>
+            <tr>
+                <td>Felhasználó név:</td>
+                <td>
+                    <asp:TextBox ID="lbUsrName_reg" runat="server"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="lbUsrName_reg" ForeColor="Red" Display="Dynamic" Text="Kötelező kitölteni"></asp:RequiredFieldValidator>
+                </td>
+            </tr>
+            <tr>
+                <td>Jelszó:</td>
+                <td>
+                    <asp:TextBox ID="lbPass1_reg" runat="server" TextMode="Password"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="lbPass1_reg" ForeColor="Red" Display="Dynamic" Text="Kötelező kitölteni"></asp:RequiredFieldValidator>
+                </td>
+            </tr>
+            <tr>
+                <td>Jelszó még egyszer:</td>
+                <td>
+                    <asp:TextBox ID="lbPass2_reg" runat="server" TextMode="Password"></asp:TextBox>
+                    <asp:RequiredFieldValidator runat="server" ControlToValidate="lbPass2_reg" ForeColor="Red" Display="Dynamic" Text="Kötelező kitölteni">
+                    </asp:RequiredFieldValidator>
+                    <asp:CompareValidator runat="server" ControlToCompare="lbPass1_reg" ControlToValidate="lbPass2_reg" ForeColor="Red" Display="Dynamic" Text="A két jelszónak egyeznie kell!">
+                    </asp:CompareValidator>
+                </td>
+            </tr>
+            <tr>
+                <td>Email cím:</td>
+                <td>
+                    <asp:TextBox ID="tbEmail" runat="server"></asp:TextBox>
+                    <asp:RequiredFieldValidator runat="server" ID="rfvTbEmail" ForeColor="Red" Text="Kötelező kitölteni!" Display="Dynamic" ControlToValidate="tbEmail"></asp:RequiredFieldValidator>
+                </td>
+            </tr>
+        </table>
+        <asp:Button ID="btnRegister" runat="server" Text="Regisztráció" OnClick="btnRegister_Click" />
+    </asp:Panel>
+
+    <asp:Panel runat="server" ID="statistics" Visible="false">
+        <h3>Játékstatisztika</h3>
+        <asp:Literal runat="server" ID="litStatistics"></asp:Literal>
+    </asp:Panel>
 </asp:Content>
